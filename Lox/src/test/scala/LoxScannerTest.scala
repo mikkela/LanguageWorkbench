babe79294ea37de03a92d7991eaff62fa7fde665
Mikkel Arentoft <mikkel@mikadocs.com>
2025-01-03 08:08:52 +0100

    Updated Identifier test

---
+++ Lox/src/test/scala/LoxScannerTest.scala
@@ -0,0 +1,31 @@
+package org.mikadocs.language.lox
+
+import org.mikadocs.language.workbench.{EndOfFileToken, SourcePosition, SourceReader}
+
+class LoxScannerTest extends munit.FunSuite{
+  // Helper for creating a simple SourcePosition
+  def createSourcePosition(l: Int, c: Int): SourcePosition = new SourcePosition {
+    override def line: Int = l
+
+    override def column: Int = c
+
+    override def lineContents: String = ""
+  }
+
+  test("Scan for identifiers") {
+    val sut = LoxScanner()
+
+    val iterator = sut.scan(SourceReader("andy formless fo _ _123 a_bc ab123\nabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_"))
+
+    assertEquals(iterator.next(), IdentifierToken("andy", createSourcePosition(1, 1)))
+    assertEquals(iterator.next(), IdentifierToken("formless", createSourcePosition(1, 6)))
+    assertEquals(iterator.next(), IdentifierToken("fo", createSourcePosition(1, 15)))
+    assertEquals(iterator.next(), IdentifierToken("_", createSourcePosition(1, 18)))
+    assertEquals(iterator.next(), IdentifierToken("_123", createSourcePosition(1, 20)))
+    assertEquals(iterator.next(), IdentifierToken("a_bc", createSourcePosition(1, 25)))
+    assertEquals(iterator.next(), IdentifierToken("ab123", createSourcePosition(1, 30)))
+    assertEquals(iterator.next(), IdentifierToken("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_", createSourcePosition(2, 1)))
+    assertEquals(iterator.next(), EndOfFileToken)
+    assertEquals(iterator.hasNext, false)
+  }
+}
