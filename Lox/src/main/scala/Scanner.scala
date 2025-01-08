7ac78bf8b4097a87eaa6e231f3b0c2d1c6db60a5
Mikkel Arentoft <mikkel@mikadocs.com>
2025-01-08 21:12:01 +0100

    Added more Lox Scanning

--- Lox/src/main/scala/Scanner.scala
+++ Lox/src/main/scala/Scanner.scala
@@ -1,7 +1,7 @@
 package org.mikadocs.language.lox
 
-import org.mikadocs.language.workbench.Acceptance.{Accepted, Rejected}
-import org.mikadocs.language.workbench.{Acceptance, RuleBasedScanner, ScannerRule, SourcePosition, Token, WhiteSpaceSkippingScanner}
+import org.mikadocs.language.workbench.Acceptance.{Accepted, Rejected, Undecided}
+import org.mikadocs.language.workbench.{Acceptance, RuleBasedScanner, StringMatchingRule, StringLookaheadMatchingRule, ScannerRule, SourcePosition, Token, WhiteSpaceSkippingScanner}
 
 private def isAlpha(c: Char): Boolean = (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_'
 private def isDigit(c: Char): Boolean = c >= '0' && c <= '9'
@@ -26,12 +26,47 @@ object IntegerNumberRule extends ScannerRule:
   override def accept(s: String): Acceptance = if """\d+""".r matches s then Acceptance.Accepted else Acceptance.Rejected
   override def apply(s: String, position: SourcePosition): Token = NumberToken(s, position)
 
-object DotRule extends ScannerRule:
-  override def accept(s: String): Acceptance = if s.equals(".") then Acceptance.Accepted else Acceptance.Rejected
-  override def apply(s: String, position: SourcePosition): Token = DotToken(s, position)
+abstract class PunctuationRule(val lexeme:String, factory: SourcePosition => Token) 
+  extends StringMatchingRule(lexeme, (s, p) => factory(p))
+
+abstract class PunctuationLoookaheadRule(val lexeme: String, factory: SourcePosition => Token)
+  extends StringLookaheadMatchingRule(lexeme, StringLookaheadMatchingRule.allSubstringsAsUndecided(lexeme), (s, p) => factory(p))
+object DotRule extends PunctuationRule(DotToken.dot, p => DotToken(p))
+object LeftParenthesisRule extends PunctuationRule(LeftParenthesisToken.leftParenthesis, p => LeftParenthesisToken(p))
+object RightParenthesisRule extends PunctuationRule(RightParenthesisToken.rightParenthesis, p => RightParenthesisToken(p))
+object LeftBraceRule extends PunctuationRule(LeftBraceToken.leftBrace, p => LeftBraceToken(p))
+object RightBraceRule extends PunctuationRule(RightBraceToken.rightBrace, p => RightBraceToken(p))
+object SemicolonRule extends PunctuationRule(SemicolonToken.semicolon, p => SemicolonToken(p))
+object CommaRule extends PunctuationRule(CommaToken.comma, p => CommaToken(p))
+object PlusRule extends PunctuationRule(PlusToken.plus, p => PlusToken(p))
+object MinusRule extends PunctuationRule(MinusToken.minus, p => MinusToken(p))
+object StarRule extends PunctuationRule(StarToken.star, p => StarToken(p))
+object SlashRule extends PunctuationRule(SlashToken.slash, p => SlashToken(p))
+object BangEqualRule extends PunctuationLoookaheadRule(BangEqualToken.bangEqual, p => BangEqualToken(p))
+object EqualEqualRule extends PunctuationLoookaheadRule(EqualEqualToken.equalEqual, p => EqualEqualToken(p))
+object LessEqualRule extends PunctuationLoookaheadRule(LessEqualToken.lessEqual, p => LessEqualToken(p))
+object GreaterEqualRule extends PunctuationLoookaheadRule(GreaterEqualToken.greaterEqual, p => GreaterEqualToken(p))
+object LessRule extends PunctuationRule(LessToken.less, p => LessToken(p))
+object GreaterRule extends PunctuationRule(GreaterToken.greater, p => GreaterToken(p))
 
 class LoxScanner extends WhiteSpaceSkippingScanner(RuleBasedScanner(Seq(
   RealNumberRule,
   IntegerNumberRule,
   DotRule,
+  LeftParenthesisRule,
+  RightParenthesisRule,
+  LeftBraceRule,
+  RightBraceRule,
+  SemicolonRule,
+  CommaRule,
+  PlusRule,
+  MinusRule,
+  StarRule,
+  SlashRule,
+  BangEqualRule,
+  EqualEqualRule,
+  LessEqualRule,
+  GreaterEqualRule,
+  LessRule,
+  GreaterRule,
   IdentifierRule)))
--- Lox/src/main/scala/Token.scala
+++ Lox/src/main/scala/Token.scala
@@ -4,4 +4,56 @@ import org.mikadocs.language.workbench.{SourcePosition, Token}
 
 case class IdentifierToken(lexeme: String, position: SourcePosition) extends Token
 case class NumberToken(lexeme: String, position: SourcePosition) extends Token
-case class DotToken(lexeme: String, position: SourcePosition) extends Token
+abstract class PunctuationToken(val lexeme: String, val position: SourcePosition) extends Token
+
+case class DotToken(override val position: SourcePosition) extends PunctuationToken(DotToken.dot, position)
+object DotToken:
+  final val dot = "."
+case class LeftParenthesisToken(override val position: SourcePosition) extends PunctuationToken(LeftParenthesisToken.leftParenthesis, position)
+object LeftParenthesisToken:
+  final val leftParenthesis = "("
+case class RightParenthesisToken(override val position: SourcePosition) extends PunctuationToken(RightParenthesisToken.rightParenthesis, position)
+object RightParenthesisToken:
+  final val rightParenthesis = ")"
+case class LeftBraceToken(override val position: SourcePosition) extends PunctuationToken(LeftBraceToken.leftBrace, position)
+object LeftBraceToken:
+  final val leftBrace = "{"
+case class RightBraceToken(override val position: SourcePosition) extends PunctuationToken(RightBraceToken.rightBrace, position)
+object RightBraceToken:
+  final val rightBrace = "}"
+case class SemicolonToken(override val position: SourcePosition) extends PunctuationToken(SemicolonToken.semicolon, position)
+object SemicolonToken:
+  final val semicolon = ";"
+case class CommaToken(override val position: SourcePosition) extends PunctuationToken(CommaToken.comma, position)
+object CommaToken:
+  final val comma = ","
+case class PlusToken(override val position: SourcePosition) extends PunctuationToken(PlusToken.plus, position)
+object PlusToken:
+  final val plus = "+"
+case class MinusToken(override val position: SourcePosition) extends PunctuationToken(MinusToken.minus, position)
+object MinusToken:
+  final val minus = "-"
+case class StarToken(override val position: SourcePosition) extends PunctuationToken(StarToken.star, position)
+object StarToken:
+  final val star = "*"
+case class BangEqualToken(override val position: SourcePosition) extends PunctuationToken(BangEqualToken.bangEqual, position)
+object BangEqualToken:
+  final val bangEqual = "!="
+case class EqualEqualToken(override val position: SourcePosition) extends PunctuationToken(EqualEqualToken.equalEqual, position)
+object EqualEqualToken:
+  final val equalEqual = "=="
+case class LessEqualToken(override val position: SourcePosition) extends PunctuationToken(LessEqualToken.lessEqual, position)
+object LessEqualToken:
+  final val lessEqual = "<="
+case class GreaterEqualToken(override val position: SourcePosition) extends PunctuationToken(GreaterEqualToken.greaterEqual, position)
+object GreaterEqualToken:
+  final val greaterEqual = ">="
+case class LessToken(override val position: SourcePosition) extends PunctuationToken(LessToken.less, position)
+object LessToken:
+  final val less = "<"
+case class GreaterToken(override val position: SourcePosition) extends PunctuationToken(GreaterToken.greater, position)
+object GreaterToken:
+  final val greater = ">"
+case class SlashToken(override val position: SourcePosition) extends PunctuationToken(SlashToken.slash, position)
+object SlashToken:
+  final val slash = "/"
