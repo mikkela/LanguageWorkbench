c38690dd2c4c713ec6531da78640f1138c844429
Mikkel Arentoft <mikkel@mikadocs.com>
2025-02-02 17:24:41 +0100

    Added Visitors to the AST

--- Workbench/src/main/scala/AST.scala
+++ Workbench/src/main/scala/AST.scala
@@ -1,3 +1,8 @@
 package org.mikadocs.language.workbench
 
 trait Node
+
+trait NodeVisitor[T]:
+  def visit(node: Node): T
+
+extension [T](node: Node) def visit(using v: NodeVisitor[T]): T = v.visit(node)
